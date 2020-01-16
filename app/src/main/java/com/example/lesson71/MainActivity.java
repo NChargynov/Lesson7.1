9996d14adde3d1cf6cf181058e593423db680043
NChargynov <n.a.chargynov.kg@gmail.com>
2020-01-17 01:45:39 +0600

    add change

--- app/src/main/java/com/example/lesson71/MainActivity.java
+++ app/src/main/java/com/example/lesson71/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.lesson71;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    boolean isButtonFragment = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.tv_result_field);
+        final Button change = findViewById(R.id.btn_change);
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isButtonFragment){
+                changeFragment(new RecyclerFragment());
+                isButtonFragment = false;
+                } else {
+                    changeFragment(new ButtonsFragment());
+                    isButtonFragment = true;
+                }
+            }
+        });
 
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, new ButtonsFragment());
-        transaction.commit();
+        changeFragment(new ButtonsFragment());
     }
 
     public void inputText(String s){
         textView.setText(s);
+    }
 
+    public void changeFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
     }
 }
